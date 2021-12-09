87954f63ceb5b87ec6f3565f40c67822124f4be8
aayushali <aayushkhan51@gmail.com>
2021-12-09 15:53:17 +0545

    user creation modal

---
+++ src/components/UserModal.vue
@@ -0,0 +1,71 @@
+<template>
+<!--  <q-dialog v-model="value" persistent>-->
+<!--    <q-card>-->
+<!--      <q-card-section class="row items-center">-->
+<!--        <q-avatar icon="signal_wifi_off" color="primary" text-color="white"/>-->
+<!--        <span class="q-ml-sm">You are currently not connected to any network.</span>-->
+<!--      </q-card-section>-->
+
+<!--      <q-card-actions align="right">-->
+<!--        <q-btn flat label="Cancel" color="primary" v-close-popup/>-->
+<!--        <q-btn flat label="Turn on Wifi" color="primary" v-close-popup/>-->
+<!--      </q-card-actions>-->
+<!--    </q-card>-->
+<!--  </q-dialog>-->
+  <q-dialog v-model="value" persistent class="dialog-box-wrapper" >
+
+    <q-card style="min-width: 600px">
+      <q-bar>
+        <q-icon name="addPerson" />
+        <div>Create New User</div>
+        <q-space />
+
+        <q-btn dense flat icon="close" v-close-popup>
+          <q-tooltip>Close</q-tooltip>
+        </q-btn>
+      </q-bar>
+      <q-card-section class="column items-center q-gutter-y-md " >
+        <q-input clearable filled color="secondary" v-model="user.name" label="Full name" style="width: 100%" />
+        <q-input v-model="user.password" color="secondary" filled :type="isPwd ? 'password' : 'text'" label="Password" style="width: 100%" >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Cancel" color="primary" v-close-popup/>
+        <q-btn flat label="Turn on Wifi" color="primary" v-close-popup/>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: "UserModal",
+  props: ['confirm'],
+  data() {
+    return {
+      isPwd: true,
+
+
+      user: {
+        name: '',
+        password: '',
+      },
+      computed : {
+        value: this.confirm,
+      }
+    },
+
+    console.log(confirm)
+
+  }
+}
+</script>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,5 +1,50 @@
 <template>
   <q-page>
+    <q-dialog v-model="confirm" persistent class="dialog-box-wrapper">
+      <q-card style="min-width: 600px">
+        <q-bar class="bg-primary q-py-lg" >
+          <q-icon name="person_add"/>
+          <div>Create New User</div>
+          <q-space/>
+          <q-btn dense flat color="negative" rounded icon="close" v-close-popup>
+            <q-tooltip>Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+        <q-card-section class="column items-center q-gutter-y-md ">
+          <q-input clearable filled color="secondary" v-model="user.fullname" label="Full name" style="width: 90%"/>
+          <q-input v-model="user.pass" color="secondary" filled :type="isPwd ? 'password' : 'text'" label="Password"
+                   style="width: 90%">
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <q-input filled clearable v-model="user.emailId" type="email"  style="width: 90%">
+            <template v-slot:prepend>
+              <q-icon name="mail"/>
+            </template>
+          </q-input>
+          <q-input
+            filled
+            clearable
+            v-model="user.mobile_num"
+            label="Phone"
+            mask="(###) ### - ####"
+            unmasked-value
+            hint="Mask: (###) ### - ####" style="width: 90%"
+          />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="negative" v-close-popup/>
+          <q-btn flat label="Create User" color="primary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-btn align="between" class="btn-fixed-width q-ml-md" color="accent" label="Create user" icon="person_add"
+           v-on:click="confirm = true"/>
     <div class="q-pa-md">
       <q-table
         class="my-sticky-header-table"
@@ -22,6 +67,15 @@ export default {
   name: "users",
   data() {
     return {
+      confirm: false,
+      user: {
+        fullname: '',
+        pass: '',
+        emailId: '',
+        mobile_num: '',
+        add: ''
+      },
+      isPwd: true,
       columns: [
         {
           name: 'id',
@@ -49,7 +103,7 @@ export default {
           sortable: true
         },
         {name: 'mobile_number', align: 'center', label: 'Mobile Number', field: 'mobile_number', sortable: true},
-        {name: 'address', label: 'Address', field: 'address', sortable: true},
+        {name: 'address', label: 'Address', field: 'address', sortable: true, align: 'center'},
         {
           name: 'email_verified_at',
           label: 'email_verified_at',
